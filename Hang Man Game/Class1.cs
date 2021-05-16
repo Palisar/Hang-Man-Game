f40a237f8328613ffa49472a0f591a6c7e438713
Palisar <83838480+Palisar@users.noreply.github.com>
2021-05-16 21:32:46 +0100

    Add project files.

---
+++ Hang Man Game/Class1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+    class Class1
+    {
+        using System;
+using System.Threading;
+
+namespace Tic_Tac_Toe
+    {
+        class Program
+        {
+            static char[] spaces = new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };  //An array to represent the spaces on the tic tac toe board
+
+            static int player = 1;
+            static int choice;  // choice will indicate what space the player choose on their turn
+            static int flag;  // used to signal when a player has won or tied
+
+            /// <summary>
+            /// Draws the game board
+            /// </summary>
+            static void DrawBoard()
+            {
+                Console.WriteLine("              |     |     ");
+                Console.WriteLine("           {0}  |  {1}  |   {2}  ", spaces[0], spaces[1], spaces[2]);
+                Console.WriteLine("         _____|_____|_____");
+                Console.WriteLine("              |     |     ");
+                Console.WriteLine("           {0}  |  {1}  |   {2}  ", spaces[3], spaces[4], spaces[5]);
+                Console.WriteLine("         _____|_____|_____");
+                Console.WriteLine("              |     |     ");
+                Console.WriteLine("           {0}  |  {1}  |   {2}  ", spaces[6], spaces[7], spaces[8]);
+                Console.WriteLine("              |     |     ");
+
+            }
+            /// <summary>
+            /// This will check if the game was won, tied, or should continue
+            /// </summary>
+            /// <returns></returns>
+            static int CheckWin()
+            {
+                if (spaces[0] == spaces[1] &&
+                    spaces[1] == spaces[2] ||   // checks row 1
+                    spaces[3] == spaces[4] &&
+                    spaces[4] == spaces[5] ||   // checks row 2
+                    spaces[6] == spaces[7] &&
+                    spaces[7] == spaces[8] ||   // checks row 3
+                    spaces[0] == spaces[3] &&
+                    spaces[3] == spaces[6] ||   // checks comlumn 1
+                    spaces[1] == spaces[4] &&
+                    spaces[4] == spaces[7] ||   // checks comlumn 2
+                    spaces[2] == spaces[5] &&
+                    spaces[5] == spaces[8] ||   // checks comlumn 3
+                    spaces[0] == spaces[4] &&
+                    spaces[4] == spaces[8] ||   // checks diagonal top left to right bottom
+                    spaces[6] == spaces[4] &&
+                    spaces[4] == spaces[2]      // checks diagonal top right to bottom left
+                    )
+                {
+                    return 1;
+                }
+                else if (spaces[0] != '1' &&
+                        spaces[1] != '2' &&
+                        spaces[2] != '3' &&
+                        spaces[3] != '4' &&
+                        spaces[4] != '5' &&
+                        spaces[5] != '6' &&
+                        spaces[6] != '7' &&
+                        spaces[7] != '8' &&
+                        spaces[8] != '9')
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 0;
+                }
+
+
+
+            }
+            /// <summary>
+            /// Draws an X on the board
+            /// </summary>
+            /// <param name="pos"></param>
+            static void DrawX(int pos)
+            {
+                spaces[pos] = 'X';
+            }
+
+            /// <summary>
+            /// Draws an O on the board
+            /// </summary>
+            /// <param name="pos"></param>
+            static void DrawO(int pos)
+            {
+                spaces[pos] = 'O';
+            }
+
+            /// <summary>
+            /// The Main Game Loop
+            /// </summary>
+            /// <param name="args"></param>
+
+            static void Main(string[] args)
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Player 1 : X \nPlayer 2 : O \n");
+                    if (player % 2 == 0)
+                    {
+                        Console.WriteLine("Player 2's turn");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Player 1's turn");
+                    }
+
+                    Console.WriteLine("\n");        // Draws a new Line to the Console  (Blank Line)
+                    DrawBoard();
+                    choice = int.Parse(Console.ReadLine()) - 1;
+                    //choice = Convert.ToInt32(Console.ReadLine()) - 1;
+
+
+                    if (spaces[choice] != 'X' &&
+                        spaces[choice] != 'O')
+                    {
+                        if (player % 2 == 0)
+                        {
+                            DrawO(choice);
+                        }
+                        else
+                        {
+                            DrawX(choice);
+                        }
+                        player++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry the row {0} is already marked with {1} \n", choice, spaces[choice]);
+                        Console.WriteLine("Please wait 2 seconds board is loading again...");
+                        Thread.Sleep(2000);
+                    }
+
+
+
+                    flag = CheckWin();
+                } while (flag != 1 && flag != -1);
+
+                if (flag == 1)
+                {
+                    Console.WriteLine("Player {0} has won!", (player % 2) + 1);
+                }
+                else
+                {
+                    Console.WriteLine("Tie Game");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
+}
---
+++ Hang Man Game/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Threading;
+
+namespace Hang_Man_Game
+{
+    class Program
+    {
+        
+        static int hangCount = 0;
+        static bool correct;
+        static bool run = true;
+        static char guessLetter;
+        static string guessWord;
+        static string checkWord;          //0    1    2    3    4    5    6   
+        static char[] wrong = new char[] { ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        static char[] fillW = new char[] { '|','O', '/', '|', '\\', '/', '\\' };
+
+        static void GameBoard()
+        {
+            Console.WriteLine("       ___      ");
+            Console.WriteLine("      |  {0}       ", wrong[0]);
+            Console.WriteLine("      |  {0}      ", wrong[1]);
+            Console.WriteLine("      | {0}{1}{2}      ",wrong[2], wrong[3], wrong[4]);
+            Console.WriteLine("      | {0} {1}      ", wrong[5], wrong[6]);
+            Console.WriteLine("      |______");
+         /*  ___
+            |  
+            |  O
+            | /|\
+            | /_\
+            ||___|
+         */
+        }
+         static int CheckWin(char[] check, char[] guess)
+        {
+            string ch = new string(check);
+            string gu = new string(guess);
+
+            if(wrong[6] == '\\')
+            {
+                return -1;
+                
+            }
+            else if(ch == gu)
+            {
+
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+             
+        }
+
+       
+        static bool GuessRight(char letter)
+        {
+            if (guessWord.Contains(letter))
+            {
+                Console.WriteLine("Correct! ");
+                Thread.Sleep(700);
+                return true;
+            }
+            else
+            {
+                
+                return false;
+            }
+        }
+       
+        
+        static void Main(string[] args)
+        {           //GAME OPENED
+            Console.WriteLine("Welcome To HangMan");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.Write("Please enter a  word: ");
+            guessWord = Console.ReadLine();
+            char[] checkArr = guessWord.ToCharArray();
+            char[] guessArr = new char[guessWord.Length ];
+            int flag = 0;
+            
+          //  --------------------------------------------
+
+            //MAIN GAME LOOP
+            do
+            {
+
+               flag = CheckWin(checkArr, guessArr);
+
+                if (flag == -1)   //GAME OVER CONDITION
+                {
+                    Console.Clear();
+                    GameBoard();
+                    Console.WriteLine("GAME OVER");   
+                    Thread.Sleep(3000);
+                    break;
+
+                }
+                else if (flag == 1)   //WIN CONDITION
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Console.WriteLine("CONGRADULATIONS YOU WIN!\n");
+                        Thread.Sleep(50);
+                        Console.Clear();
+                        Console.WriteLine("\nCONGRADULATIONS YOU WIN!");
+                        Thread.Sleep(50);
+                        Console.Clear();
+                        Console.WriteLine("\n \nCONGRADULATIONS YOU WIN!");
+                        Thread.Sleep(50);
+                        Console.Clear();
+                    }
+                    Console.WriteLine("CONGRADULATIONS YOU WIN!");
+                    Thread.Sleep(3000);
+                    break;
+                }
+
+
+                Console.Clear();
+                Console.WriteLine(guessArr);
+                GameBoard();
+                Console.Write("Please enter a letter: ");
+
+                string str = Console.ReadLine();
+                while(str.Length != 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine(guessArr);
+                    GameBoard();
+                    Console.Write("Invalid Input. Please enter a letter: ");
+                    str = Console.ReadLine();
+                }
+                guessLetter = Convert.ToChar(str);
+
+                correct = GuessRight(guessLetter);
+
+                if (correct)
+                {
+                    for (int i = 0; i < guessWord.Length  ; i++)
+                    {
+                        if(guessLetter == checkArr[i])
+                        {
+                            guessArr[i] = guessLetter;      
+                        }
+                     
+                    }   
+                }
+                else
+                {
+                    wrong[hangCount] = fillW[hangCount];
+                    Console.WriteLine("Incorrect!");
+                    Thread.Sleep(700);
+                    hangCount++;
+
+                }
+
+            } while (run);
+
+            
+        }
+    }
+}
