36d1e962a7f7df466354b5c0eec45a641a0c3c02
Palisar <83838480+Palisar@users.noreply.github.com>
2021-06-18 16:39:07 +0100

    Large ReWork of the code. Added the HangMan Object to run the games functions and cleaned up the code in the main class. Further cleaning up of the code can be done . Resolved the issue of the last letter not appearing when the player wins the game.

---
+++ Hang Man Game/HangMan.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hang_Man_Game
+{
+    class HangMan
+    {
+        private string guessWord;
+        public string GuessWord { 
+            get { return guessWord; }
+            set { guessWord = value; } 
+        }                               //0    1    2    3    4    5    6   
+        private static char[] wrong = new char[] { ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        private static char[] fillW = new char[] { '|', 'O', '/', '|', '\\', '/', '\\' };
+        private static int hangCount = 0;
+        public static void GameBoard()
+        {
+            Console.WriteLine("       ___      ");
+            Console.WriteLine("      |  {0}       ", wrong[0]);
+            Console.WriteLine("      |  {0}      ", wrong[1]);
+            Console.WriteLine("      | {0}{1}{2}      ", wrong[2], wrong[3], wrong[4]);
+            Console.WriteLine("      | {0} {1}      ", wrong[5], wrong[6]);
+            Console.WriteLine("      |______");
+
+        }
+        public int CheckWin(char[] check, char[] guess, char[] blank)
+        {
+            string checking = new string(check);
+            string guessing = new string(guess);
+
+            if (wrong[6] == '\\')
+            {
+                Console.Clear();
+                Console.WriteLine(blank);
+                HangMan.GameBoard();
+                Console.WriteLine("GAME OVER");
+                Thread.Sleep(3000);
+               return -1;
+
+            }
+            else if (checking == guessing)
+            {
+                Console.Clear();
+                Console.WriteLine(blank);
+                HangMan.GameBoard();
+                Console.WriteLine("CONGRADULATIONS YOU WIN!");
+                Thread.Sleep(3000);
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        public bool CheckForNumsAndSpaces()
+        {
+            if (guessWord.Any(char.IsDigit)|| guessWord.Any(char.IsSeparator)) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool GuessRight(char letter, string guessWord)
+        {
+            if (guessWord.Contains(letter))
+            {
+                Console.WriteLine("\nCorrect! ");
+                Thread.Sleep(500);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("\nIncorrect! ");
+                Thread.Sleep(500);
+                wrong[hangCount] = fillW[hangCount];
+                hangCount++;
+                return false;
+            }
+        }
+    }
+}
--- Hang Man Game/Program.cs
+++ Hang Man Game/Program.cs
@@ -6,167 +6,76 @@ namespace Hang_Man_Game
 {
     class Program
     {
-        
-        static int hangCount = 0;
-        static bool correct;
-        static char guessLetter;
-        static string guessWord;
-        static string checkWord;          //0    1    2    3    4    5    6   
-        static char[] wrong = new char[] { ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
-        static char[] fillW = new char[] { '|','O', '/', '|', '\\', '/', '\\' };
-
-        static void GameBoard()
-        {
-            Console.WriteLine("       ___      ");
-            Console.WriteLine("      |  {0}       ", wrong[0]);
-            Console.WriteLine("      |  {0}      ", wrong[1]);
-            Console.WriteLine("      | {0}{1}{2}      ",wrong[2], wrong[3], wrong[4]);
-            Console.WriteLine("      | {0} {1}      ", wrong[5], wrong[6]);
-            Console.WriteLine("      |______");
-         /*  ___
-            |  
-            | \O/
-            |  |
-            | /_\
-            ||___|
-         */
-        }
-         static int CheckWin(char[] check, char[] guess)
-        {
-            string ch = new string(check);
-            string gu = new string(guess);
-
-            if(wrong[6] == '\\')
-            {
-                return -1;
-                
-            }
-            else if(ch == gu)
-            {
-
-                return 1;
-            }
-            else
-            {
-                return 0;
-            }    
-        }
-
-       
-        static bool GuessRight(char letter)
-        {
-            if (guessWord.Contains(letter))
-            {
-                Console.WriteLine("Correct! ");
-                Thread.Sleep(700);
-                return true;
-            }
-            else
-            {
-                
-                return false;
-            }
-        }
-       
-        
         static void Main(string[] args)
-        {           //GAME OPENED
+        {           //---GAME OPENED---
+            HangMan Game = new HangMan();
+            char[] blankArr;
+            char[] checkArr;
+            char[] guessArr;    
+
             Console.WriteLine("Welcome To HangMan");
-            Thread.Sleep(2000);
+            Thread.Sleep(1000);
             Console.Clear();
             Console.Write("Please enter a  word: ");
-            guessWord = Console.ReadLine();
+            Game.GuessWord = Console.ReadLine().ToLower();
             /*Added this While Loop to make sure the person entering a word
              does not use numbers.*/
-            bool noNumbers = guessWord.Any(char.IsDigit);
-            while (noNumbers)
+            
+            while (Game.CheckForNumsAndSpaces())
             {
                 Console.Clear();
                 Console.WriteLine("Word must not contain Numbers.");
                 Console.Write("Please enter a  word: ");
-                guessWord = Console.ReadLine();
-                noNumbers = guessWord.Any(char.IsDigit);
+                Game.GuessWord = Console.ReadLine().ToLower() ;
             }
-            char[] blankArr = new char[guessWord.Length];
-            char[] checkArr = guessWord.ToCharArray();
-            char[] guessArr = new char[guessWord.Length ];
+
+            blankArr = new char[Game.GuessWord.Length];
+            checkArr = Game.GuessWord.ToCharArray();
+            guessArr = new char[Game.GuessWord.Length];
             int flag = 0;
 
             /* added this fucntion to present the word as a blank string of '*'
                in the game to help player know how many characters they are dealing with 
                and where they have guessed correctly */
-            for (int c = 0; c < checkArr.Length; c++)
+            for(int i = 0; i < Game.GuessWord.Length; i++)
             {
-                blankArr[c] = '*';
+                blankArr[i] = '*';
             }
-          //  --------------------------------------------
+            //  ----------------------------------------------------------------------------------------
 
             //MAIN GAME LOOP
             do
             {
-
-               flag = CheckWin(checkArr, guessArr);
-
-                if (flag == -1)   //GAME OVER CONDITION
-                {
-                    Console.Clear();
-                    GameBoard();
-                    Console.WriteLine("GAME OVER");   
-                    Thread.Sleep(3000);
-                    break;
-
-                }
-                else if (flag == 1)   //WIN CONDITION
-                {
-                   
-                    Console.WriteLine("CONGRADULATIONS YOU WIN!");
-                    Thread.Sleep(3000);
-                    break;
-                }
-
-
                 Console.Clear();
                 Console.WriteLine(blankArr);
-                GameBoard();
+                HangMan.GameBoard();
                 Console.Write("Please enter a letter: ");
-
-                bool intCheck = false;  //Added This to check for numbers
-                string str = Console.ReadLine();
-                intCheck = str.Any(char.IsDigit);
-                while (str.Length != 1 ||  intCheck)      // This While Loop is just to check if the user inputs a single letter and also if its a number
+                char key = Console.ReadKey(false).KeyChar;
+                while (char.IsDigit(key))      // This While Loop is just to check if the user inputs a single letter and also if its a number
                 {
                     Console.Clear();
                     Console.WriteLine(blankArr);
-                    GameBoard();
-                    intCheck = false;
+                    HangMan.GameBoard();
                     Console.Write("Invalid Input. Please enter a letter: ");
-                    str = Console.ReadLine();
-                    intCheck = str.Any(char.IsDigit);
+                    key = Console.ReadKey(false).KeyChar;
+                    
                 }
-                guessLetter = Convert.ToChar(str);
 
-                correct = GuessRight(guessLetter);
-
-                if (correct)
+                if (Game.GuessRight(key, Game.GuessWord))
                 {
-                    for (int i = 0; i < guessWord.Length  ; i++)
+                    for (int i = 0; i < Game.GuessWord.Length  ; i++)
                     {
-                        if(guessLetter == checkArr[i])
+                        if(key == checkArr[i])
                         {
-                            guessArr[i] = guessLetter;
-                            blankArr[i] = guessLetter;
+                            guessArr[i] = key;
+                            blankArr[i] = key;
                         }
                      
                     }   
                 }
-                else
-                {
-                    wrong[hangCount] = fillW[hangCount];
-                    Console.WriteLine("Incorrect!");
-                    Thread.Sleep(500);
-                    hangCount++;
 
-                }
+                //Check if the game is Won/Lost/Still going
+                flag = Game.CheckWin(checkArr, guessArr, blankArr);
 
             } while (flag == 0);
 
